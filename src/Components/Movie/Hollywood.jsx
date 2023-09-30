66e72f9c5028629360b4426e222123afe37d8f74
singhprince12346 <114760573+singhprince12346@users.noreply.github.com>
2023-10-01 01:01:49 +0530

    Added Avatar movie

--- src/Components/Movie/Hollywood.jsx
+++ src/Components/Movie/Hollywood.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, Button } from "antd";
 
 import Inception from '../../images/inception.jpg';
+import Avatar from '../../images/avatar.jpg';
 
 const Hollywood = () => {
   return (
@@ -30,7 +31,48 @@ const Hollywood = () => {
                 alignItems: "center",
               }}
             >
+               <a
+            href='https://github.com/vikhyatsingh123'
+            target="_blank"
+            rel="noopener noreferrer"
+          >
               <Button type="primary">Contributed by Vikhyat Singh</Button>
+                 </a>
+            </div>
+          </div>
+        </div>
+      </Card>
+       {/* Card 2 */}
+      <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img
+              src={Avatar}
+              alt="Image"
+              style={{ maxWidth: "100%", height: "150px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>Avatar</h3>
+            <p>
+            Jake Sully and Ney'tiri have formed a family and are doing everything to stay together.
+            However, they must leave their home and explore the regions of Pandora. When an ancient 
+            threat resurfaces, Jake must fight a difficult war against the humans.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+               <a
+            href='https://github.com/singhprince12346'
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+              <Button type="primary">Contributed by Prince Singh</Button>
+                 </a>
             </div>
           </div>
         </div>
