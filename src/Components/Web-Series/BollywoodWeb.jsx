a420aba9c4e7d343125b8ad77b85f6e2587b3697
Aeyaz <aeyazadil123@gmail.com>
2023-12-07 19:13:30 +0530

    Added Sacred Games to Bollywood webseries

--- src/Components/Web-Series/BollywoodWeb.jsx
+++ src/Components/Web-Series/BollywoodWeb.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, Button } from "antd";
 
 import Scam from '../../images/scam.jpg';
+import SacredGames from '../../images/SacredGames.jpg';
 
 const BollywoodWeb = () => {
   return (
@@ -35,6 +36,42 @@ const BollywoodWeb = () => {
           </div>
         </div>
       </Card>
+        {/* CARD 2 */}
+        <Card>
+            <div style={{ display: "flex", alignItems: "center" }}>
+                <div style={{ flex: 1 }}>
+                    <img
+                        src={SacredGames}
+                        alt="Image"
+                        style={{ maxWidth: "100%", height: "150px" }}
+                    />
+                </div>
+                <div style={{ flex: 6 }}>
+                    <h3>Sacred Games</h3>
+                    <p>
+                        When police officer Sartaj Singh receives an anonymous tip
+                        about the location of criminal overlord Ganesh Gaitonde,
+                        he embarks on a chase around Mumbai in what becomes a
+                        dangerous cat-and-mouse game.
+                    </p>
+                    <div
+                        style={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        <a
+                            href='https://github.com/aeyazadil'
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <Button type="primary">Contributed by Aeyaz Adil</Button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </Card>
     </>
   )
 };
