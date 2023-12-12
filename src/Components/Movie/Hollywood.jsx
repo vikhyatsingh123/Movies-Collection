11128f750cc2023ca97effdb1b4089ca0efd0a66
YashSachan2 <yash.sachan.ece22@itbhu.ac.in>
2023-12-12 23:07:38 +0530

    Added Dark Knight Movie Card

--- src/Components/Movie/Hollywood.jsx
+++ src/Components/Movie/Hollywood.jsx
@@ -6,6 +6,7 @@ import Avatar from '../../images/avatar.jpg';
 import Dune from '../../images/dune.jpg';
 import EndGame from '../../images/end.jpg';
 import Memento from '../../images/Memento.jpg'
+import dark_knight from '../../images/dark_knight.jpg'
 
 
 const Hollywood = () => {
@@ -35,20 +36,20 @@ const Hollywood = () => {
                 alignItems: "center",
               }}
             >
-               <a
-            href='https://github.com/vikhyatsingh123'
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-              <Button type="primary">Contributed by Vikhyat Singh</Button>
-                 </a>
+              <a
+                href='https://github.com/vikhyatsingh123'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button type="primary">Contributed by Vikhyat Singh</Button>
+              </a>
             </div>
           </div>
         </div>
       </Card>
 
 
-       {/* Card 2 */}
+      {/* Card 2 */}
       <Card>
         <div style={{ display: "flex", alignItems: "center" }}>
           <div style={{ flex: 1 }}>
@@ -61,9 +62,9 @@ const Hollywood = () => {
           <div style={{ flex: 6 }}>
             <h3>Avatar</h3>
             <p>
-            Jake Sully and Ney'tiri have formed a family and are doing everything to stay together.
-            However, they must leave their home and explore the regions of Pandora. When an ancient 
-            threat resurfaces, Jake must fight a difficult war against the humans.
+              Jake Sully and Ney'tiri have formed a family and are doing everything to stay together.
+              However, they must leave their home and explore the regions of Pandora. When an ancient
+              threat resurfaces, Jake must fight a difficult war against the humans.
             </p>
             <div
               style={{
@@ -72,20 +73,20 @@ const Hollywood = () => {
                 alignItems: "center",
               }}
             >
-               <a
-            href='https://github.com/singhprince12346'
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-              <Button type="primary">Contributed by Prince Singh</Button>
-                 </a>
+              <a
+                href='https://github.com/singhprince12346'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button type="primary">Contributed by Prince Singh</Button>
+              </a>
             </div>
           </div>
         </div>
       </Card>
 
 
-       {/* Card 3 */}
+      {/* Card 3 */}
       <Card>
         <div style={{ display: "flex", alignItems: "center" }}>
           <div style={{ flex: 1 }}>
@@ -98,9 +99,9 @@ const Hollywood = () => {
           <div style={{ flex: 6 }}>
             <h3>Dune</h3>
             <p>
-            American epic science fiction film directed by Denis Villeneuve, who co-wrote the screenplay with Jon Spaihts and Eric Roth.
-            Set in the far distant future, in a post-artificial intelligence universe, the film follows Paul Atreides as his family, 
-            the noble House Atreides, is thrust into a war for the deadly and inhospitable desert planet Arrakis.
+              American epic science fiction film directed by Denis Villeneuve, who co-wrote the screenplay with Jon Spaihts and Eric Roth.
+              Set in the far distant future, in a post-artificial intelligence universe, the film follows Paul Atreides as his family,
+              the noble House Atreides, is thrust into a war for the deadly and inhospitable desert planet Arrakis.
             </p>
             <div
               style={{
@@ -109,13 +110,13 @@ const Hollywood = () => {
                 alignItems: "center",
               }}
             >
-               <a
-            href='https://github.com/Blaster2398'
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-              <Button type="primary">Contributed by Aryan Patel</Button>
-                 </a>
+              <a
+                href='https://github.com/Blaster2398'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button type="primary">Contributed by Aryan Patel</Button>
+              </a>
             </div>
           </div>
         </div>
@@ -135,8 +136,8 @@ const Hollywood = () => {
           <div style={{ flex: 6 }}>
             <h3>End Game</h3>
             <p>
-            After Thanos, an intergalactic warlord, disintegrates half of the universe, the Avengers must reunite and assemble again
-             to reinvigorate their trounced allies and restore balance.
+              After Thanos, an intergalactic warlord, disintegrates half of the universe, the Avengers must reunite and assemble again
+              to reinvigorate their trounced allies and restore balance.
             </p>
             <div
               style={{
@@ -145,50 +146,81 @@ const Hollywood = () => {
                 alignItems: "center",
               }}
             >
-               <a
-            href='https://github.com/sami3160'
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-              <Button type="primary">Contributed by Sami Bhadgaonkar</Button>
-                 </a>
+              <a
+                href='https://github.com/sami3160'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button type="primary">Contributed by Sami Bhadgaonkar</Button>
+              </a>
             </div>
           </div>
         </div>
       </Card>
       {/* CARD_NUMBER 5 */}
-   <Card>
+      <Card>
         <div style={{ display: "flex", alignItems: "center" }}>
           <div style={{ flex: 1 }}>
             <img src={Memento}
-           alt="Memento"
-           style={{ maxWidth: "100%", height: "150px" }}
-         />
-       </div>
-       <div style={{ flex: 6 }}>
-         <h3>Memento</h3>
-         <p>
-         Memento's story follows Shelby, who has short-term memory loss following a break-in at his house in which his wife was raped and murdered and a blow to the head that gave Shelby his "condition." Leonard has been chasing the guy who did it, but his search is complicated by the fact that he can't make new memories since the accident.
-         </p>
-         <div
-           style={{
-             display: "flex",
-             justifyContent: "center",
-             alignItems: "center",
-           }}
-         >
-           <a
-             href='https://github.com/saurabhgdeshmukh'
-             target="_blank"
-             rel="noopener noreferrer"
-           >
-             <Button type="primary">Contributed by Saurabh Deshmukh</Button>
-           </a>
-         </div>
-       </div>
-     </div>
-   </Card>
-      
+              alt="Memento"
+              style={{ maxWidth: "100%", height: "150px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>Memento</h3>
+            <p>
+              Memento's story follows Shelby, who has short-term memory loss following a break-in at his house in which his wife was raped and murdered and a blow to the head that gave Shelby his "condition." Leonard has been chasing the guy who did it, but his search is complicated by the fact that he can't make new memories since the accident.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <a
+                href='https://github.com/saurabhgdeshmukh'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button type="primary">Contributed by Saurabh Deshmukh</Button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Card>
+      {/* CARD_NUMBER 6*/}
+      <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img src={dark_knight}
+              alt="Memento"
+              style={{height: "150px",width:"100px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>The Dark Knight</h3>
+            <p>
+              In the second part of Batman Triology,Batman has a new foe, the Joker, who is an accomplished criminal hell-bent on decimating Gotham City. Together with Gordon and Harvey Dent, Batman struggles to thwart the Joker before it is too late.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <a
+                href='https://github.com/YashSachan2'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button type="primary">Contributed by Yash Sachan</Button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Card>
     </>
   );
 };
