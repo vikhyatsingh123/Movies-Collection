9557743aac700e13bc473165f77522808d4682c2
singhcoder694 <vivekkr694@gmail.com>
2023-12-19 09:58:40 +0530

    Added movies in Bollywood and Tollywood Section

--- src/Components/Movie/Bollywood.jsx
+++ src/Components/Movie/Bollywood.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Card, Button } from "antd";
 
 import War from '../../images/war.jpg';
+import Animal from '../../images/Animal.png';
 
 const Bollywood = () => {
   return (
@@ -32,6 +33,34 @@ const Bollywood = () => {
               }}
             >
               <Button type="btn btn-outline-dark gitbtn">Contributed by Vikhyat Singh</Button>
+            </div>
+          </div>
+        </div>
+      </Card>
+      <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img
+              src={Animal}
+              alt="Image"
+              style={{ maxWidth: "100%", height: "150px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>Animal</h3>
+            <p>
+              Movie depicting an emotional relation between father and son.
+              Ranbir Kapoor as son is ready to do anything for his Father and
+              this leads to bruttle fight scenes.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Button type="btn btn-outline-dark gitbtn">Contributed by Vivek Singh</Button>
             </div>
           </div>
         </div>
