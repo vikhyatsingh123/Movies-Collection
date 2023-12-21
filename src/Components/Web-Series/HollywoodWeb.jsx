329316b89c2de417f33ff3940ce1e28b7e2d7416
Surajit Maity <sibu23122003@gmail.com>
2023-12-21 18:18:33 +0530

    Add Peaky Blinders card

--- src/Components/Web-Series/HollywoodWeb.jsx
+++ src/Components/Web-Series/HollywoodWeb.jsx
@@ -4,6 +4,7 @@ import Suits from '../../images/Suits.jpg'
 import Got from '../../images/Got.jpg';
 import Witcher from '../../images/Witcher.jpg'
 import MoneyHiest from '../../images/moneyH.jpg'
+import PeakyBlinders from '../../images/PeakyBlinders.png'
 const HollywoodWeb = () => {
   return (
     <>
@@ -37,8 +38,8 @@ const HollywoodWeb = () => {
         </div>
       </Card>
 
-      
-       {/* Card 2 */}
+
+      {/* Card 2 */}
       <Card>
         <div style={{ display: "flex", alignItems: "center" }}>
           <div style={{ flex: 1 }}>
@@ -51,7 +52,7 @@ const HollywoodWeb = () => {
           <div style={{ flex: 6 }}>
             <h3>The Witcher</h3>
             <p>
-             Geralt of Rivia, a solitary monster hunter, struggles to find his place 
+              Geralt of Rivia, a solitary monster hunter, struggles to find his place
               in a world where people often prove more wicked than beasts.
             </p>
             <div
@@ -61,13 +62,13 @@ const HollywoodWeb = () => {
                 alignItems: "center",
               }}
             >
-                  <a
-            href='https://github.com/PrinceSingh12398'
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-              <Button type="btn btn-outline-dark gitbtn">Contributed by Prince Singh</Button>
-                    </a>
+              <a
+                href='https://github.com/PrinceSingh12398'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button type="btn btn-outline-dark gitbtn">Contributed by Prince Singh</Button>
+              </a>
             </div>
           </div>
         </div>
@@ -86,9 +87,9 @@ const HollywoodWeb = () => {
           <div style={{ flex: 6 }}>
             <h3>Money Heist</h3>
             <p>
-            The group takes 67 hostages, including the daughter of the British Ambassador. 
-            They lock themselves in the Royal Mint for over a week. Their goal is to print
-             €2.4 billion in unmarked bills without bloodshed. 
+              The group takes 67 hostages, including the daughter of the British Ambassador.
+              They lock themselves in the Royal Mint for over a week. Their goal is to print
+              €2.4 billion in unmarked bills without bloodshed.
             </p>
             <div
               style={{
@@ -97,10 +98,10 @@ const HollywoodWeb = () => {
                 alignItems: "center",
               }}
             ><a href="https://github.com/sami3160"
-            target="_blank"
-            rel="noopener noreferrer">
-              <Button type="btn btn-outline-dark gitbtn">Contributed by Sami Bhadgaonkar</Button>
-            </a>
+              target="_blank"
+              rel="noopener noreferrer">
+                <Button type="btn btn-outline-dark gitbtn">Contributed by Sami Bhadgaonkar</Button>
+              </a>
             </div>
           </div>
         </div>
@@ -110,39 +111,69 @@ const HollywoodWeb = () => {
         <div style={{ display: "flex", alignItems: "center" }}>
           <div style={{ flex: 1 }}>
             <img src={Suits}
-           alt="SUITS"
-           style={{ maxWidth: "100%", height: "150px" }}
-         />
-       </div>
-       <div style={{ flex: 6 }}>
-         <h3>Suits</h3>
-         <p>
-         "Suits" follows brilliant college dropout Mike Ross, 
-         hired by top lawyer Harvey Specter, despite lacking a 
-         law degree. The duo navigates legal challenges at 
-         Pearson Specter, combining wit and deception. 
-         The series explores complex relationships and high-stakes 
-         legal drama in a fast-paced corporate world.
-         </p>
-         <div
-           style={{
-             display: "flex",
-             justifyContent: "center",
-             alignItems: "center",
-           }}
-         >
-           <a
-             href='https://github.com/aindree-2005'
-             target="_blank"
-             rel="noopener noreferrer"
-           >
-             <Button type="btn btn-outline-dark gitbtn">Contributed by Aindree Chatterjee</Button>
-           </a>
-         </div>
-       </div>
-     </div>
-   </Card>
-   
+              alt="SUITS"
+              style={{ maxWidth: "100%", height: "150px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>Suits</h3>
+            <p>
+              "Suits" follows brilliant college dropout Mike Ross,
+              hired by top lawyer Harvey Specter, despite lacking a
+              law degree. The duo navigates legal challenges at
+              Pearson Specter, combining wit and deception.
+              The series explores complex relationships and high-stakes
+              legal drama in a fast-paced corporate world.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <a
+                href='https://github.com/aindree-2005'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button type="btn btn-outline-dark gitbtn">Contributed by Aindree Chatterjee</Button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+      {/* Card 5 */}
+      <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img
+              src={PeakyBlinders}
+              alt="PeakyBlinders"
+              style={{ maxWidth: "100%", height: "150px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>Peaky Blinders</h3>
+            <p>
+            Peaky Blinders is a British period crime drama television series created by Steven Knight.
+             Set in Birmingham, it follows the exploits of the Peaky Blinders crime gang in the direct
+              aftermath of the First World War. The fictional gang is loosely based on a real urban youth
+               gang of the same name who were active in the city from the 1880s to the 1910s.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <Button type="btn btn-outline-dark gitbtn">Contributed by Surajit Maity</Button>
+            </div>
+          </div>
+        </div>
+      </Card>
 
     </>
   );
