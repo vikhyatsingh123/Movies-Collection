5fa6a78f1320f734730b7afa7f584eb36e9c8ec4
Priyansh S Shrivastav <priyanshs18171819@gmail.com>
2023-12-25 19:10:18 +0530

    Changed Navbar + Added Webseries + Added Anime Section

--- src/Components/Web-Series/BollywoodWeb.jsx
+++ src/Components/Web-Series/BollywoodWeb.jsx
@@ -4,8 +4,11 @@ import { Card, Button } from "antd";
 import Scam from '../../images/scam.jpg';
 import FamilyMan from '../../images/FamilyMan.jpg';
 import SacredGames from '../../images/SacredGames.jpg';
-
-
+import mirzapur from '../../images/mirzapur.jpg';
+import scam1992 from '../../images/scam1992.jpg';
+import aspirants from '../../images/aspirants.jpg';
+import panchayat from '../../images/panchayat.jpeg';
+import gullak from '../../images/gullak.avif';
 const BollywoodWeb = () => {
   return (
     <>
@@ -107,6 +110,151 @@ const BollywoodWeb = () => {
                 </div>
             </div>
         </Card>
+        <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img
+              src={mirzapur}
+              alt="Image"
+              style={{ maxWidth: "100%", height: "150px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>Mirzapur</h3>
+            <p>
+            A shocking incident at a wedding procession ignites a series of events entangling the lives of two families in the lawless city of Mirzapur.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            ><a href="https://github.com/PSS2134"
+            target="_blank"
+            rel="noopener noreferrer">
+              <Button type="btn btn-outline-dark gitbtn">Contributed by PSS2134</Button>
+            </a>
+            </div>
+          </div>
+        </div>
+      </Card>
+      <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img
+              src={scam1992}
+              alt="Image"
+              style={{ maxWidth: "100%", height: "150px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>Scam 1992: The Harshad Mehta Story</h3>
+            <p>
+            The rise and fall of Harshad Mehta, a stockbroker who single-handedly took the stock market to great heights, is depicted. 
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            ><a href="https://github.com/PSS2134"
+            target="_blank"
+            rel="noopener noreferrer">
+              <Button type="btn btn-outline-dark gitbtn">Contributed by PSS2134</Button>
+            </a>
+            </div>
+          </div>
+        </div>
+      </Card>
+      <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img
+              src={aspirants}
+              alt="Image"
+              style={{ maxWidth: "100%", height: "150px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>Aspirants</h3>
+            <p>
+            It is the story of the journey of three UPSC (public service competitive exams in India) aspirants and their friendship against all odds. 
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            ><a href="https://github.com/PSS2134"
+            target="_blank"
+            rel="noopener noreferrer">
+              <Button type="btn btn-outline-dark gitbtn">Contributed by PSS2134</Button>
+            </a>
+            </div>
+          </div>
+        </div>
+      </Card>
+      <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img
+              src={panchayat}
+              alt="Image"
+              style={{ maxWidth: "100%", height: "150px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>Panchayat</h3>
+            <p>
+            A comedy-drama, which captures the journey of an engineering graduate Abhishek, who for lack of a better job option joins as secretary of a Panchayat office in a remote village of Uttar Pradesh. 
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            ><a href="https://github.com/PSS2134"
+            target="_blank"
+            rel="noopener noreferrer">
+              <Button type="btn btn-outline-dark gitbtn">Contributed by PSS2134</Button>
+            </a>
+            </div>
+          </div>
+        </div>
+      </Card>
+      <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img
+              src={gullak}
+              alt="Image"
+              style={{ maxWidth: "70%", height: "150px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>Gullak</h3>
+            <p>
+            Set in quaint by-lanes in the heart of India, Gullak is a collection of disarming and relatable tales of the Mishra family.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            ><a href="https://github.com/PSS2134"
+            target="_blank"
+            rel="noopener noreferrer">
+              <Button type="btn btn-outline-dark gitbtn">Contributed by PSS2134</Button>
+            </a>
+            </div>
+          </div>
+        </div>
+      </Card>
     </>
   )
 };
