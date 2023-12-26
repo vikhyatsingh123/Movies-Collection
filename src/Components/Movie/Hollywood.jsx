37b1635b123480ec603408689f920e26888c7070
Surajit Maity <sibu23122003@gmail.com>
2023-12-26 21:05:38 +0530

    50 new cards added

--- src/Components/Movie/Hollywood.jsx
+++ src/Components/Movie/Hollywood.jsx
@@ -7,7 +7,16 @@ import Dune from '../../images/dune.jpg';
 import EndGame from '../../images/end.jpg';
 import Memento from '../../images/Memento.jpg'
 import dark_knight from '../../images/dark_knight.jpg'
-
+import avater2 from '../../images/avater2.png'
+import Oppenheimer from '../../images/Oppenheimer.png'
+import Barbie from '../../images/Barbie.png'
+import GOTG3 from '../../images/GOTG3.png'
+import JW4 from '../../images/JW4.png'
+import MI7 from '../../images/MI7.png'
+import AM3 from '../../images/AM3.png'
+import TRB from '../../images/TRB.png'
+import Shazam2 from '../../images/Shazam2.png'
+import Blue_Beetle from '../../images/Blue_Beetle.png'
 
 const Hollywood = () => {
   return (
@@ -53,14 +62,14 @@ const Hollywood = () => {
 
       {/* Card 2 */}
       <Card >
-        <div style={{ display: "flex", alignItems: "center" ,padding : "0"}}>
-          <div className="fade-img" style={{ flex: 1,maxWidth: "100%", height: "100%" /*border:"1px red solid"*/ ,margin: "0"}}>
+        <div style={{ display: "flex", alignItems: "center", padding: "0" }}>
+          <div className="fade-img" style={{ flex: 1, maxWidth: "100%", height: "100%" /*border:"1px red solid"*/, margin: "0" }}>
             <img
-            className="img"
+              className="img"
               src={Avatar}
               alt="Image"
               style={{ maxWidth: "100%", height: "150px", /*borderRadius: "10px"*/ }}
-         
+
             />
           </div>
           <div style={{ flex: 6 }}>
@@ -199,7 +208,7 @@ const Hollywood = () => {
           <div style={{ flex: 1 }}>
             <img src={dark_knight}
               alt="Memento"
-              style={{height: "150px",width:"100px" }}
+              style={{ height: "150px", width: "100px" }}
             />
           </div>
           <div style={{ flex: 6 }}>
@@ -220,6 +229,340 @@ const Hollywood = () => {
                 rel="noopener noreferrer"
               >
                 <button className="btn btn-outline-dark gitbtn">Contributed by Yash Sachan</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+      {/* CARD_NUMBER 7*/}
+      <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img src={avater2}
+              alt="Avatar: The Way of Water"
+              style={{ height: "150px", width: "100px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>Avatar: The Way of Water</h3>
+            <p>
+              Jake Sully lives with his newfound family formed on the extrasolar moon Pandora. Once a familiar threat returns to finish what was previously started, Jake must work with Neytiri and the army of the Na'vi race to protect their home.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <a
+                href='https://github.com/Surajit0573'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="btn btn-outline-dark gitbtn">Contributed by Surajit Maity</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+      {/* CARD_NUMBER 8*/}
+      <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img src={Oppenheimer}
+              alt="Oppenheimer"
+              style={{ height: "150px", width: "100px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>Oppenheimer</h3>
+            <p>
+              During World War II, Lt. Gen. Leslie Groves Jr. appoints physicist J. Robert Oppenheimer to work on the top-secret Manhattan Project. Oppenheimer and a team of scientists spend years developing and designing the atomic bomb. Their work comes to fruition on July 16, 1945, as they witness the world's first nuclear explosion, forever changing the course of history.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <a
+                href='https://github.com/Surajit0573'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="btn btn-outline-dark gitbtn">Contributed by Surajit Maity</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+      {/* CARD_NUMBER 9*/}
+      <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img src={Barbie}
+              alt="Barbie"
+              style={{ height: "150px", width: "100px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>Barbie</h3>
+            <p>
+              Barbie and Ken are having the time of their lives in the colorful and seemingly perfect world of Barbie Land. However, when they get a chance to go to the real world, they soon discover the joys and perils of living among humans.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <a
+                href='https://github.com/Surajit0573'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="btn btn-outline-dark gitbtn">Contributed by Surajit Maity</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+      {/* CARD_NUMBER 10*/}
+      <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img src={GOTG3}
+              alt="GOTG3"
+              style={{ height: "150px", width: "100px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>Guardians of the Galaxy Vol. 3</h3>
+            <p>
+              Still reeling from the loss of Gamora, Peter Quill must rally his team to defend the universe and protect one of their own. If the mission is not completely successful, it could possibly lead to the end of the Guardians as we know them.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <a
+                href='https://github.com/Surajit0573'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="btn btn-outline-dark gitbtn">Contributed by Surajit Maity</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+      {/* CARD_NUMBER 11*/}
+      <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img src={JW4}
+              alt="JW4"
+              style={{ height: "150px", width: "100px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>John Wick: Chapter 4</h3>
+            <p>
+              With the price on his head ever increasing, legendary hit man John Wick takes his fight against the High Table global as he seeks out the most powerful players in the underworld, from New York to Paris to Japan to Berlin.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <a
+                href='https://github.com/Surajit0573'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="btn btn-outline-dark gitbtn">Contributed by Surajit Maity</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+      {/* CARD_NUMBER 12*/}
+      <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img src={MI7}
+              alt="MI7"
+              style={{ height: "150px", width: "100px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>
+              Mission: Impossible – Dead Reckoning Part One</h3>
+            <p>
+              Ethan Hunt and the IMF team must track down a terrifying new weapon that threatens all of humanity if it falls into the wrong hands. With control of the future and the fate of the world at stake, a deadly race around the globe begins. Confronted by a mysterious, all-powerful enemy, Ethan is forced to consider that nothing can matter more than the mission -- not even the lives of those he cares about most.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <a
+                href='https://github.com/Surajit0573'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="btn btn-outline-dark gitbtn">Contributed by Surajit Maity</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+        {/* CARD_NUMBER 13*/}
+        <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img src={AM3}
+              alt="AM3"
+              style={{ height: "150px", width: "100px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>
+            Ant-Man and the Wasp: Quantumania</h3>
+            <p>
+            Ant-Man and the Wasp find themselves exploring the Quantum Realm, interacting with strange new creatures and embarking on an adventure that pushes them beyond the limits of what they thought was possible.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <a
+                href='https://github.com/Surajit0573'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="btn btn-outline-dark gitbtn">Contributed by Surajit Maity</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+        {/* CARD_NUMBER 14*/}
+        <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img src={TRB}
+              alt="TRB"
+              style={{ height: "150px", width: "100px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>
+            Transformers: Rise of the Beasts</h3>
+            <p>
+            Optimus Prime and the Autobots team up with a down on his luck young man, an aspiring historian and with a powerful faction of Transformers known as the Maximals to combat a sinister force from outer space that threatens the Earth and all of mankind.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <a
+                href='https://github.com/Surajit0573'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="btn btn-outline-dark gitbtn">Contributed by Surajit Maity</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Card>
+
+          {/* CARD_NUMBER 15*/}
+          <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img src={Shazam2}
+              alt="Shazam2"
+              style={{ height: "150px", width: "100px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>
+            Shazam! Fury of the Gods</h3>
+            <p>
+            Shazam and his superpowered foster siblings band together to face the Daughters of Atlas, who have stolen the Wizard's staff and plan to use it for their own ends.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <a
+                href='https://github.com/Surajit0573'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="btn btn-outline-dark gitbtn">Contributed by Surajit Maity</button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </Card>
+       {/* CARD_NUMBER 16*/}
+       <Card>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <div style={{ flex: 1 }}>
+            <img src={Blue_Beetle}
+              alt="Blue Beetle"
+              style={{ height: "150px", width: "100px" }}
+            />
+          </div>
+          <div style={{ flex: 6 }}>
+            <h3>
+            Blue Beetle</h3>
+            <p>
+            Jaime Reyes suddenly finds himself in possession of an ancient relic of alien biotechnology called the Scarab. When the Scarab chooses Jaime to be its symbiotic host, he's bestowed with an incredible suit of armor that's capable of extraordinary and unpredictable powers, forever changing his destiny as he becomes the superhero Blue Beetle.
+            </p>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <a
+                href='https://github.com/Surajit0573'
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button className="btn btn-outline-dark gitbtn">Contributed by Surajit Maity</button>
               </a>
             </div>
           </div>
